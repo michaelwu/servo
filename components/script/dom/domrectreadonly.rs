29246b764f469466630abfbfabf2e64f4886efd4
Michael Wu <mwu@mozilla.com>
2015-08-18 16:46:14 -0400

    Avoid Wrap imports

--- components/script/dom/domrectreadonly.rs
+++ components/script/dom/domrectreadonly.rs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use dom::bindings::codegen::Bindings::DOMRectReadOnlyBinding::{DOMRectReadOnlyMethods, Wrap};
+use dom::bindings::codegen::Bindings::DOMRectReadOnlyBinding::DOMRectReadOnlyMethods;
 use dom::bindings::error::Fallible;
 use dom::bindings::global::GlobalRef;
 use dom::bindings::js::Root;
